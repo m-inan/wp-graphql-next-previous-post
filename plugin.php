a2279cb0a0f042cffb56538c90e022a3941d631d
minan <muhammet@tilpark.com>
2020-03-21 00:40:04 +0300

    initial commit

---
+++ plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: WPGraphQL Next Previous Post
+ * Plugin URI: https://github.com/m-inan/wp-graphql-next-previous-post
+ * Description: Adds post type for next and previous post field
+ * Author: Minan
+ * Version: 0.0.1
+ *
+ */
+
+// To make this plugin work properly for both Composer users and non-composer
+// users we must detect whether the project is using a global autolaoder. We
+// can do that by checking whether our autoloadable classes will autoload with
+// class_exists(). If not it means there's no global autoloader in place and
+// the user is not using composer. In that case we can safely require the
+// bundled autoloader code.
+if (!\class_exists('\WPGraphQL\Extensions\NextPreviousPost')) {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
+
+// Load the actual plugin code
+\WPGraphQL\Extensions\NextPreviousPost\Loader::init();
---
+++ src/Loader.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace WPGraphQL\Extensions\NextPreviousPost;
+
+use WPGraphQL\AppContext;
+use WPGraphQL\Data\DataSource;
+use WPGraphQL\Model\Post;
+
+/**
+ * Class Laoder
+ *
+ * This class allows you to see the next and previous posts in the 'post' type.
+ *
+ * @package WNextPreviousPost
+ * @since   0.0.1
+ */
+class Loader
+{
+    public static function init()
+    {
+        define('WP_GRAPHQL_NEXT_PREVIOUS_POST', 'initialized');
+        (new Loader())->bind_hooks();
+    }
+
+    public function bind_hooks()
+    {
+        add_action(
+            'graphql_register_types',
+            [$this, 'npp_action_register_types'],
+            9,
+            0
+        );
+
+    }
+
+    public function npp_action_register_types()
+    {
+        register_graphql_field('Post', 'next', [
+            'type' => 'post',
+            'description' => __(
+                'Get Next Post',
+                'wp-graphql-offset-pagination'
+            ),
+            'resolve' => function (Post $post, array $args, AppContext $context) {
+                $next = get_next_post();
+
+                return DataSource::resolve_post_object($next->ID, $context);
+            },
+        ]);
+
+        register_graphql_field('Post', 'previous', [
+            'type' => 'post',
+            'description' => __(
+                'Get Previous Post',
+                'wp-graphql-offset-pagination'
+            ),
+
+            'resolve' => function (Post $post, array $args, AppContext $context) {
+                $prev = get_previous_post();
+
+                return DataSource::resolve_post_object($prev->ID, $context);
+            },
+        ]);
+    }
+}
